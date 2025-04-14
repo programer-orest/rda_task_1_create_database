7b054a5f84d5f3aa2df2f31c744bc494f65ad0c7
Orest Yakymiv <akimivorest@gmail.com>
2025-04-14 18:37:27 +0300

    DataBase

--- task.sql
+++ task.sql
@@ -1,1 +1,41 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE DATABASE ShopDB;
+
+CREATE TABLE Products(
+  ProductID INT AUTO_INCREMENT,
+  Name VARCHAR(50),
+  Description VARCHAR(50),
+  Price INT,
+  WarehouseAmount INT,
+  PRIMARY KEY (ProductID)
+);
+
+CREATE TABLE BonusAccounts (
+    EmployeeID INT PRIMARY KEY,
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Customers(
+  CustomersID INT AUTO_INCREMENT,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Email VARCHAR(50),
+  Address VARCHAR(50),
+  PRIMARY KEY(CustomersID)
+);
+
+CREATE TABLE Orders(
+  OrdersID INT AUTO_INCREMENT,
+  CustomerID INT,
+  FOREIGN KEY(CustomerID) REFERENCES Customers(CustomersID) ON DELETE SET NULL,
+  Date Date,
+  PRIMARY KEY(OrdersID)
+);
+
+CREATE TABLE OrderItems(
+  OrderItemID INT AUTO_INCREMENT,
+  OrderID INT,
+  FOREIGN KEY(OrderID) REFERENCES Orders(OrdersID),
+  ProductID INT,
+  FOREIGN KEY(ProductID) REFERENCES Products(ProductID),
+  PRIMARY KEY(OrderItemID)
+);
